133e6fe43ec3d71c570aa3b5409b162572ebd6fb
cyberhawk01 <oyelakinakintunde04@gmail.com>
2026-04-14 10:14:28 -0700

    post controller

---
+++ ScriptBackend/backend/src/routes/post.route.js
@@ -0,0 +1,10 @@
+import {Router} from 'express'
+import { createPost } from '../controller/post.controller.js'
+
+
+const router = Router()
+
+router.route('/create').post(createPost);
+
+
+export default router;
